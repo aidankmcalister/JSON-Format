fe62b45321199ee466ebad42e473e2fb96b42808
Aidan <aidankmcalister@gmail.com>
2023-10-13 15:56:27 -0500

    copy button

--- scripts.js
+++ scripts.js
@@ -1,15 +1,44 @@
 const input = document.querySelector("#input-text");
 const output = document.querySelector("#output-text");
+const errorMsgElement = document.querySelector("#errorMsg");
 
 function getInfo() {
-  const lines = input.value.split("\n");
-  const formattedLines = lines
-    .filter((line) => line.trim() !== "")
-    .map((line) => {
-      const parts = line.split(":");
-      return `  "${parts[0]}": "${parts[1]}"`;
+  if (input.value.trim() === "") {
+    console.log("empty");
+    errorMsgElement.style.display = "block";
+  } else {
+    errorMsgElement.style.display = "none";
+    const lines = input.value.split("\n");
+    const formattedLines = lines
+      .filter((line) => line.trim() !== "")
+      .map((line) => {
+        const parts = line.split(/[:|]/);
+        return `<span class="cyan-text">"${parts[0]}"</span>: <span class="orange-text">"${parts[1]}"</span>`;
+      });
+
+    formattedLines.forEach((line, index) => {
+      formattedLines[index] = `  ${line}`;
     });
-  const textFormatted = `{\n${formattedLines.join("\n")}\n}`;
-  console.log(textFormatted);
-  output.textContent = textFormatted;
+
+    const textFormatted = `{<br>${formattedLines.join(",<br>")}<br>}`;
+    output.innerHTML = textFormatted;
+  }
+}
+
+function copyText() {
+  const formattedText = `{${output.textContent.trim()}}`;
+  const textToCopy = formattedText.slice(1, -1);
+  const textarea = document.createElement("textarea");
+  textarea.id = "temp-element";
+  textarea.style.height = 0;
+  document.body.appendChild(textarea);
+  textarea.value = textToCopy;
+  const selector = textarea;
+  selector.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+}
+
+function eraseText() {
+  input.textContent = "";
 }
